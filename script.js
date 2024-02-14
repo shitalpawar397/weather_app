bb2da5795d47d80962730c4fa29edf89042f09e2
Shital Pawar <shitalpawar397@gmail.com>
2024-02-15 00:16:42 +0530

    updated

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ async function fetchWeatherData(cityName){
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3e2bbbcf43efebec18f7574b7df2a471`;
 	let response = await fetch(url);
 	const data = await response.json();
+	// console.log(data);
+	let city = document.getElementById("city");
+	city.innerHTML = data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML = data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity + "g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp) - 273.15) + "°C";
 	console.log(data);
+
+
 }
 // fetchWeatherData();
 
